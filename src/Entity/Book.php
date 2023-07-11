c4fe7e7ba60c61a7d9e6dfce8ced806f3c9e5e48
emertovaa <e.mertovaa@gmail.com>
2023-07-11 17:12:02 +0200

    init

---
+++ src/Entity/Book.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BookRepository::class)]
+class Book
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $kniha = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $autor = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $rok = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getKniha(): ?string
+    {
+        return $this->kniha;
+    }
+
+    public function setKniha(string $kniha): static
+    {
+        $this->kniha = $kniha;
+
+        return $this;
+    }
+
+    public function getAutor(): ?string
+    {
+        return $this->autor;
+    }
+
+    public function setAutor(string $autor): static
+    {
+        $this->autor = $autor;
+
+        return $this;
+    }
+
+    public function getRok(): ?\DateTimeInterface
+    {
+        return $this->rok;
+    }
+
+    public function setRok(?\DateTimeInterface $rok): static
+    {
+        $this->rok = $rok;
+
+        return $this;
+    }
+}
