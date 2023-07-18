8deee3b94d390f5d017fdba9babffe50220c9f9b
emertovaa <e.mertovaa@gmail.com>
2023-07-18 11:29:23 +0200

    clean up database

---
+++ migrations/Version20230718092358.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230718092358 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE book DROP FOREIGN KEY FK_CBE5A33114D45BBE');
+        $this->addSql('DROP INDEX IDX_CBE5A33114D45BBE ON book');
+        $this->addSql('ALTER TABLE book DROP autor_id, DROP knihy_id');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE book ADD autor_id INT DEFAULT NULL, ADD knihy_id INT NOT NULL');
+        $this->addSql('ALTER TABLE book ADD CONSTRAINT FK_CBE5A33114D45BBE FOREIGN KEY (autor_id) REFERENCES autor (id)');
+        $this->addSql('CREATE INDEX IDX_CBE5A33114D45BBE ON book (autor_id)');
+    }
+}
